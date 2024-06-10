5c737314dccfb93e4cc18f5f9ca2c3afe906d955
Vince Salvino <salvino@coderedcorp.com>
2024-06-10 16:36:49 -0400

    Upgrade to wagtail 6.1

--- home/models.py
+++ home/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
     pass
 
-    ### STEP 2 ####
+    ### STEP 2 ###
     #body = RichTextField(blank=True)
     #
     #content_panels = Page.content_panels + [
@@ -43,7 +42,7 @@ class HomePage(Page):
 #    content_panels = Page.content_panels + [
 #        FieldPanel('date'),
 #        FieldPanel('intro'),
-#        ImageChooserPanel('image'),
+#        FieldPanel('image'),
 #        FieldPanel('body'),
 #    ]
 #
--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -4,12 +4,12 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_wk&x+!4@g%mryuoh5fn$tsm!0td1%!gob6%=qjm6(rah43ae*'
+SECRET_KEY = "django-insecure-q#s@_%1aq7c+l!b42!^x#job)w!6f_86jiw8_l8p344vk&ffnq"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 try:
